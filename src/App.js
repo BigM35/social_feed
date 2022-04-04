17d508ecf25e88994a3a24d31b83d394ba63a623
Manny M <bigm@Mannys-MacBook-Pro.local>
2022-04-04 17:04:00 -0500

    created component folder + created and wrote logic for CreatePost component + created DisplayPost component(still needs to finish logic) + import create post to App.js and instantiate it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CreatePost from './Components/CreatePost/CreatePost';
 
 
 function App() {
   return (
-    <div className="App">
-    hello world
+    <div>
+      <CreatePost />
     </div>
   );
 }
