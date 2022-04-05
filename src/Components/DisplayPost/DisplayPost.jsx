8c0b4ff0c1a9300e7f8fc85ee9f7233467c528c4
Manny M <bigm@Mannys-MacBook-Pro.local>
2022-04-05 16:05:32 -0500

    added LikeButton to DisplayPost in order to display a like button with each post being displayed

--- src/Components/DisplayPost/DisplayPost.jsx
+++ src/Components/DisplayPost/DisplayPost.jsx
@@ -1,18 +1,19 @@
+import LikeButton from "../LikeButton/LikeButton";
 
 
 const DisplayPost = (props) => {
     return (
         <table>
-            
             <tbody>
                 {props.parentEntries.map((entry, index)=> {
                         return (
                             <tr key={index}>
                                 <td>{entry.name}</td>
                                 <td>{entry.post}</td>
+                                <td>{<LikeButton/>}</td>
                             </tr>
                         )
-                    })};
+                    })}
                
             </tbody>
         </table>
